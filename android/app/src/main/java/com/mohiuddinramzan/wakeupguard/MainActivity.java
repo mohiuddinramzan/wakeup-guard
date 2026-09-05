e5d54f1ab7889daedd12bf2d3ff90d0ddd9fa752
mohiuddinramzan <mohiuddinramzan2@gmail.com>
2026-09-05 18:10:15 +0600

    Auto-grant WebView media permission requests for mic/camera

--- android/app/src/main/java/com/mohiuddinramzan/wakeupguard/MainActivity.java
+++ android/app/src/main/java/com/mohiuddinramzan/wakeupguard/MainActivity.java
@@ -1,5 +1,19 @@
 package com.mohiuddinramzan.wakeupguard;
 
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.bridge.getWebView().setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                runOnUiThread(() -> request.grant(request.getResources()));
+            }
+        });
+    }
+}
